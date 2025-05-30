5891caa1209df0d0fc927e504e1163cfe6c43de0
shivPosted <shivpratap621@gmail.com>
2025-05-31 03:22:23 +0530

    configuring express to handle request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(
+  express.json({
+    limit: "24kb",
+  }),
+);
+
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true, limit: "24kb" }));
+
+import userRouter from "./routes/user.route.js";
+
+app.use("/api/v1/popcorn/user", userRouter); //NOTE: tranfering controll to userRouter
 
 export default app;
