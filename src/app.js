314507b41a27450881a3b1f6e8c6a351f323c162
shivPosted <shivpratap621@gmail.com>
2025-06-11 01:45:57 +0530

    I am a fool 😆😆

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ import userRouter from "./routes/user.route.js";
 app.use("/api/v1/popcorn/users", userRouter); //NOTE: tranfering controll to userRouter
 
 //NOTE: for handling errors for the frontend
-app.use((err, _, res) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
   res.status(statusCode).json({
